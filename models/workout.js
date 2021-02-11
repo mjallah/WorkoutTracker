f1a176c0dc2424f723037c871f12dbee9d3e6784
Malik Jallah <malikjallah@gmail.com>
2021-02-10 19:28:14 -0600

    finished public and workout folders

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+            },
+            name: {
+                type: String,
+            },
+            weight: {
+                type: Number,
+            },
+            duration: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+        },
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        console.log(total, exercise);
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
